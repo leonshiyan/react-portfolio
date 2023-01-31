1bac888bcafa851e2c575ffd7b9558b730cc3c68
leonshiyan <leonshiyan@gmail.com>
2023-01-30 21:25:43 -0800

    add ProjectPreview component

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,10 +1,13 @@
 import projects from "../data/projects"
+import ProjectPreview from "./ProjectPreview"
 const ProjectList = (props) => {
   return (
     <>
       <ul>
         {projects.map((project, index) => (
-          <li key={index}>{project.title}</li>
+          <li key={index}>
+            <ProjectPreview title={project.title} image={project.image}/>
+          </li>
         ))}
       </ul>
     </>
