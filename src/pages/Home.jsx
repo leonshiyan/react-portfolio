f0f8dd21a2b9e5555bc81b8beaa49f813a2e9bd5
leonshiyan <leonshiyan@gmail.com>
2023-01-31 22:07:30 -0800

    index page style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,21 @@
 import NavBar from "../components/NavBar"
+import { Container, Row, Col } from 'react-bootstrap'
+import Image from 'react-bootstrap/Image'
 
 const Home = (props) => {
   return (
     <>
       <NavBar />
-      <h1>Welcome to my Page</h1>
-      <div>
-        <div>My name is Yan Shi</div>
-        <div>A software engineer</div>
-        <div>
-          <img src="pics/rsz_mypic.jpg" alt="mypicture" width="600" height="450"/>
-        </div>
-      </div>
+      <Container fluid="md" style={{ backgroundColor: "#388", width: "100vw" ,height : "100vh"}}>
+      <Row>
+        <Col className="my-5">
+            <h1 className="my-2">Welcome to my Page</h1>
+            <h3 className="my-2">My name is Yan Shi</h3>
+            <h4 className="my-2">A software engineer</h4>
+            <Image src="pics/rsz_mypic.jpg" alt="mypicture" />
+        </Col>
+      </Row>
+      </Container>
     </>
   )
 }
