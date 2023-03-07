3639d56196c800169ce7d92e0bdaf1a46a3dac7f
leonshiyan <leonshiyan@gmail.com>
2023-03-06 21:05:29 -0800

    css update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,27 +7,24 @@ const Home = (props) => {
     <>
       <NavBar />
       <main className={styles.container}>
-        <section id="about">
+        <div className={styles.about}>
           <header>
             <h1 >HI,I'M YAN SHI</h1>
             <h1 >A FULLSTACK DEVELOPER</h1>
             <h1 >BASED IN CANADA</h1>
           </header>
           <p>
-            Full-stack developer with a strong desire to learn and grow in the industry.
-          </p>
-          <p>
-            Specializing in modern technologies and frameworks such as MERN stack.
-          </p>
-          <p>
+            Full-stack developer with a strong desire to learn and grow in the industry. <br />
+            Specializing in modern technologies and frameworks such as MERN stack.<br />
             Always looking for opportunities to expand skills and gain knowledge about new technologies.
           </p>
-        </section>
-        <div id="projects">
+        </div>
+        <div ></div>
+        <div className={styles.projects}>
           <h1>Projects</h1>
           <ProjectList />
         </div>
-        <section id="contact">
+        <div id="contact">
           <h1>Contact</h1>
           <button class="btn btn-primary">
             <a href="mailto:leonshiyan@email.com">Send Email</a>
@@ -41,7 +38,7 @@ const Home = (props) => {
             <a href=" https://github.com/leonshiyan">Github</a>
           </button>
           <br />
-        </section>
+        </div>
       </main>
     </>
   )
