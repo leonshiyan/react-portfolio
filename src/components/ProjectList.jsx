00b02edf46e103d9622ed46b647842607786a267
leonshiyan <leonshiyan@gmail.com>
2023-03-06 21:45:38 -0800

    css update

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,31 +1,68 @@
-import projects from "../data/projects"
-import Card from 'react-bootstrap/Card'
+import { useState } from 'react';
+import projects from "../data/projects";
+import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
-
 const ProjectList = (props) => {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  const handleMouseEnter = (index) => {
+    setHoveredIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredIndex(null);
+  };
+
   return (
     <>
-    <Row xs={1} md={3} className="g-4">
-      {projects.map((project, index) => (
-        <Col>
-          <Card key={index} bg="dark">
-            <Card.Title>{project.title}</Card.Title>
-            <Card.Text>{project.description}</Card.Text>
-            <Card.Img variant="top" src={project.image} alt={project.title}/>
-              <a href={project.repositoryLink}>
-                <button class="btn btn-primary">GitHub Repository</button>
-              </a>
-              <br />
-              <a href={project.deploymentLink}>
-                <button class="btn btn-primary">Deployed Site</button>
-              </a>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+      <Row xs={1} md={3} className="g-4">
+        {projects.map((project, index) => (
+          <Col key={index}>
+            <Card
+              bg="dark"
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={() => handleMouseLeave()}
+              style={{ position: 'relative' }}
+            >
+              <Card.Title>{project.title}</Card.Title>
+              <Card.Img variant="top" src={project.image} alt={project.title} />
+              {hoveredIndex === index && (
+                <div
+                  style={{
+                    position: 'absolute',
+                    top: 50,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexDirection: 'column',
+                    textAlign: 'center',
+                    color: 'white',
+                    fontSize: '1.5rem',
+                  }}
+                >
+                  <div>{project.description}</div>
+                  <div>
+                    <a href={project.repositoryLink}>
+                      <button className="btn btn-primary">GitHub Repository</button>
+                    </a>
+                    <a href={project.deploymentLink}>
+                      <button className="btn btn-primary">Deployed Site</button>
+                    </a>
+                  </div>
+                </div>
+              )}
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </>
-  )
-}
-export default ProjectList
+  );
+};
+
+export default ProjectList;
